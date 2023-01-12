497f981622390b81b4f92728b9c06df04e96f987
evgeny_mordyasov <michisig11i7@gmail.com>
2023-01-12 20:05:31 +0700

    add illegal state code

---
+++ rgo-cloud-common-api/src/main/java/rgo/cloud/common/api/exception/IllegalStateException.java
@@ -0,0 +1,14 @@
+package rgo.cloud.common.api.exception;
+
+import rgo.cloud.common.api.rest.StatusCode;
+
+public class IllegalStateException extends CoreException {
+    public IllegalStateException(String message) {
+        super(message);
+    }
+
+    @Override
+    public StatusCode code() {
+        return StatusCode.ILLEGAL_STATE;
+    }
+}
--- rgo-cloud-common-api/src/main/java/rgo/cloud/common/api/rest/StatusCode.java
+++ rgo-cloud-common-api/src/main/java/rgo/cloud/common/api/rest/StatusCode.java
@@ -3,6 +3,7 @@ package rgo.cloud.common.api.rest;
 public enum StatusCode {
     SUCCESS,
     INVALID_RQ,
+    ILLEGAL_STATE,
     ENTITY_NOT_FOUND,
     VIOLATES_CONSTRAINT,
     UNAUTHORIZED,
